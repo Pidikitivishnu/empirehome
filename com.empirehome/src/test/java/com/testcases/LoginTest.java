0039fbad0f4fa27fdd10c894b0cf7fbb80312d86
Admin <Admin@192.168.1.35>
2023-10-19 20:25:18 -1200

    vishnu

---
+++ com.empirehome/src/test/java/com/testcases/LoginTest.java
@@ -0,0 +1,50 @@
+package com.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+import com.base.Testbase;
+import com.page.Login_functionality;
+
+public class LoginTest extends Testbase {
+
+	Login_functionality lf;
+
+	public LoginTest() throws Throwable {
+		super();
+	}
+
+	@BeforeMethod
+
+	public void setup() throws Throwable {
+
+		Initialization();
+
+		lf = new Login_functionality(driver);
+
+	}
+
+	@Test
+
+	public void Verifylogin() {
+
+		lf.Dologin();
+
+		String url = driver.getCurrentUrl();
+
+		Assert.assertEquals("http://empirehome.myprojectsonline.co.in/EmpireHome/", url);
+
+	}
+
+	@AfterMethod
+
+	public void teardown() {
+
+		//driver.close();
+
+	}
+
+}
